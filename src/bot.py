78b91a06e73d38bfa8ece317690ecb5d22ae1ee3
mohammed mubarak <modymu9@gmail.com>
2023-04-18 07:16:07 +0200

    refactored and cleaned bot library

--- src/bot.py
+++ src/bot.py
@@ -1,19 +1,19 @@
+#########
 #!/bin/env/python3
 
 import os
 import poe
-import logging
-import sys
 import threading
 import time
+import logging
 
 client = poe.Client(os.getenv("POE_TOKEN"))
 
 # poe.logger.setLevel(logging.INFO)
 
-# uncomment this for production only - Note: only one message per day is allowed for GPT-4
-#model = "beaver" #GPT-4
-model = "chinchilla" # ChatGPT
+# Uncomment this for production only - Note: only one message per day is allowed for GPT-4
+# model = "beaver" # GPT-4
+model = "chinchilla"  # ChatGPT
 
 class bcolors:
 	HEADER = '\033[95m'
@@ -29,57 +29,27 @@ class bcolors:
 
 class Query:
 	def __init__(self, message="who are you ?"):
-		self.message = message
-		self.description = message
-		self.age = None
-		self.gender = None
-		self.weight = None
-		self.height = None
-		self.bp = None # blood pressure
-		self.hr = None # heart rate
-
+	    	self.message = message
+	    	self.description = message
+	    	self.params = {}
+	
 	def set_message(self, message):
 		self.message = message
 		self.description = message
 		return self.message
-
-	def set_age(self, age):
-		text = f"My age is: {age}"
-		self.age = text
-		return self.age
-
-	def set_gender(self, gender):
-		text = f"My gender type is: {gender}"
-		self.gender = text
-		return self.message
-
-	def set_weight(self, weight):
-		text = f"My weight is: {weight}"
-		self.weight = text
-		return self.weight
-
-	def set_height(self, height):
-		text = f"My height is: {height}"
-		self.height = text
-		return self.height
-
-	def set_bp(self, bp):
-		text = f"My blood pressure is: {bp}"
-		self.bp = text
-		return self.bp
-
-	def Create(self):
-		params = [self.age, self.gender, self.weight, self.height, self.bp, self.hr]
+	
+	def set_param(self, key, value):
+		text = f"My {key} is: {value}"
+		self.params[key] = text
+	
+	def create_message(self):
 		text = []
-		for value in params:
-			if (value != None):
-				print(value)
-				text.append(str(value))
-		text = ", ".join(text) + '\n'
-		text += self.message
-		self.message = text
+		for value in self.params.values():
+			text.append(value)
+		text.append(self.message)
+		self.message = ", ".join(text) + '\n'
 		return self
-
+	
 	def get_message(self):
 		return self.description
 
@@ -89,11 +59,11 @@ class Reply:
 		self.query = query
 		self.response = None
 		self.thread_count = 0
-
+	
 	def send(self, interactive=False):
 		self.response = client.send_message(model, self.query.message, with_chat_break=True)
 		print(f"{bcolors.OKCYAN}Prompt >> {bcolors.ENDC}", self.query.get_message())
-		if (interactive == True):
+		if interactive:
 			for i in self.response:
 				print(i["text_new"], end="", flush=True)
 		else:
@@ -101,52 +71,51 @@ class Reply:
 				pass
 			print(i["text"])
 		print()
-		self.purge()
-
-	# handle multiple users
+		return (i["text"])
+	
 	def message_thread(self, desc, prompt, counter):
 		for i in client.send_message(model, prompt, with_chat_break=True):
 			pass
 		print(f"{bcolors.OKCYAN}Prompt >> {bcolors.ENDC}", end="")
-		print(desc+"\n"+i["text"]+"\n"+"-"*100+"\n")
+		print(desc + "\n" + i["text"] + "\n" + "-" * 100 + "\n")
 		self.thread_count -= 1
-
+	
 	def send_multiple(self, *queries):
-		for i in range(len(queries)):
-			t = threading.Thread(target=self.message_thread, args=(queries[i].get_message(), queries[i].message, i), daemon=True)
+		for query in queries:
+			t = threading.Thread(target=self.message_thread, args=(query.get_message(),
+						query.message, self.thread_count), daemon=True)
 			t.start()
 			self.thread_count += 1
 		
 		while self.thread_count:
 			time.sleep(0.1)
-
+		
 		print()
 		self.purge()
-
+	
 	def purge(self):
 		client.purge_conversation(model)
 
+
 if __name__ == "__main__":
-	############## 
+	##############
 	# Test Cases #
 	##############
-
+	
 	# Query will build the message
 	text = "I have headache and some pain on my neck"
 	ki2kid = Query(text)
-	ki2kid.set_age(22)
-	ki2kid.set_gender("male")
-
+	ki2kid.set_param("age", 22)
+	ki2kid.set_param("gender", "male")
+	
 	text2 = "I can't see clearly"
 	mano = Query(text2)
-	mano.set_age(15)
-	mano.set_gender("male")
-
+	mano.set_param("age", 15)
+	mano.set_param("gender", "male")
+	
 	# Create the message
-	ki2kid.Create()
-	mano.Create()
-
+	ki2kid.create_message()
 	# Reply takes a Query and return a response
 	answer = Reply(ki2kid)
-	answer.send(interactive=True)
-	answer.send_multiple(ki2kid, mano)
+	rep = answer.send(interactive=True)
+	print(rep)
--- src/main.py
+++ src/main.py
@@ -2,14 +2,11 @@
 from bot import Query, Reply
 
 mubarak = Query("I have a headache on the left side of my head with some pain on the upper left part of my neck")
-mubarak.set_age = 22
-mubarak.set_gender = "male"
-Reply(mubarak.Create()).send(interactive=True)
+mubarak.set_param("age", 22)
+mubarak.set_param("gender", "male")
 
 
 while (True):
 	print("Describe you problem")
 	mubarak.set_message(input())
-	Reply(mubarak.Create()).send(interactive=True)
-#mubarak.set_message("I think my left arm is broken")
-#Reply(mubarak.Create()).send(interactive=True)
+	Reply(mubarak.create_message()).send(interactive=True)
