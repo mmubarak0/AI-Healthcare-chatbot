c980c7b76c3a4734265412438c7a0d0b02313f86
mohammed mubarak <modymu9@gmail.com>
2023-04-18 09:42:38 +0200

    integrate bot logic into gui

--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -1,39 +1,79 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QMenuBar, QAction, QStatusBar, QMessageBox, QInputDialog, QMainWindow
+from src.bot import Query, Reply
 
-class MyHealthCareBot(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
+class MyHealthCareBot(QMainWindow):
+	def __init__(self):
+		super().__init__()
+		self.initUI()
+		self.query = Query()
+	
+	def initUI(self):
+		self.setWindowTitle('AI-Powered HealthCare Chatbot')
+		self.setGeometry(100, 100, 600, 400)
+		
+		# Create widgets
+		self.label = QLabel('Enter patient description:')
+		self.submit_button = QPushButton('Submit')
+		self.output_label = QLabel()
+		self.output_label.setWordWrap(True)
+		self.input_text = QLineEdit()
+		
+		# Create menu bar
+		menu_bar = self.menuBar()
+		options_menu = menu_bar.addMenu('Options')
 
-    def initUI(self):
-        self.setWindowTitle('AI-Powered HealthCare Chatbot')
-        self.setGeometry(100, 100, 400, 200)
+		# Create actions for menu bar
+		set_age_action = QAction('Set Age', self)
+		set_age_action.triggered.connect(self.show_set_age_dialog)
+		options_menu.addAction(set_age_action)
 
-        # Create widgets
-        self.label = QLabel('Enter patient description:')
-        self.input_text = QLineEdit()
-        self.submit_button = QPushButton('Submit')
-        self.output_label = QLabel()
+		set_gender_action = QAction('Set Gender', self)
+		set_gender_action.triggered.connect(self.show_set_gender_dialog)
+		options_menu.addAction(set_gender_action)
+		# Create status bar
+		self.status_bar = QStatusBar()
+		self.setStatusBar(self.status_bar)
 
-        # Create layout
-        layout = QVBoxLayout()
-        layout.addWidget(self.label)
-        layout.addWidget(self.input_text)
-        layout.addWidget(self.submit_button)
-        layout.addWidget(self.output_label)
+		# Create layout
+		layout = QVBoxLayout()
+		layout.addWidget(self.label)
+		layout.addWidget(self.input_text)
+		layout.addWidget(self.submit_button)
+		layout.addWidget(self.output_label)
 
-        # Set layout for the window
-        self.setLayout(layout)
+		# Create central widget
+		central_widget = QWidget()
+		central_widget.setLayout(layout)
+		# Set central widget
+		self.setCentralWidget(central_widget)
 
-        # Connect button click event to slot
-        self.submit_button.clicked.connect(self.process_input)
+		# Set layout for the window
+		self.setLayout(layout)
+		
+		# Connect button click event to slot
+		self.submit_button.clicked.connect(self.process_input)
 
-    def process_input(self):
-        # Get user input from the input_text QLineEdit
-        user_input = self.input_text.text()
+	def show_set_age_dialog(self):
+		age, ok = QInputDialog.getInt(self, 'Set Age', 'Enter age:', 0, 0, 150)
+		if ok:
+			self.query.set_param("age", str(age))
+			self.status_bar.showMessage('Age set to: {}'.format(age))
 
-        # chatbot's logic goes here
-        bot_output = f"Bot Output: {user_input}"
+	def show_set_gender_dialog(self):
+		gender, ok = QInputDialog.getText(self, 'Set Gender', 'Enter gender (e.g. male, female):')
+		if ok:
+			self.query.set_param("gender", gender)
+			self.status_bar.showMessage('Gender set to: {}'.format(gender))
 
-        # Update the output_label QLabel with the bot's output
-        self.output_label.setText(bot_output)
+	def process_input(self):
+		# Get user input from the input_text QLineEdit
+		user_input = self.input_text.text()
+
+		# update query message
+		self.query.set_message(user_input)
+
+		# chatbot's logic goes here
+		bot_output = Reply(self.query.create_message()).send(True)
+
+		# Update the output_label QLabel with the bot's output
+		self.output_label.setText(bot_output)
